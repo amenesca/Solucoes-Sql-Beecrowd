3eb546367bc62ca44aab8e9195be76381d697517
Alan Menescal Cáceres <66499569+amenesca@users.noreply.github.com>
2023-10-18 17:53:30 -0300

    Update ExecutiveRepresentatives.sql

--- ExecutiveRepresentatives.sql
+++ ExecutiveRepresentatives.sql
@@ -1,6 +1,8 @@
 /*The financial sector needs a report on the providers of the products we sell. The reports include all categories, but for some reason, providers of products whose category is the executive, are not in the report.
 
-Your job is to return the names of the products and providers whose category ID is 6.*/
+Your job is to return the names of the products and providers whose category ID is 6.
+
+Os dados e tabelas estao no beecrowd*/
 
 SELECT products."name", providers."name"
 FROM products
