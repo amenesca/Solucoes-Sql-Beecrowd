93083794da4637c6c14553d633781d4ed00e6bbf
Alan Menescal Cáceres <66499569+amenesca@users.noreply.github.com>
2023-10-18 17:53:14 -0300

    Update Categories.sql

--- Categories.sql
+++ Categories.sql
@@ -1,7 +1,9 @@
 /*
 When the data were migrated to the database, there was a small misunderstanding on the DBA.
 
-Your boss needs you to select the ID and the name of the products, whose categorie name start with 'super'.*/
+Your boss needs you to select the ID and the name of the products, whose categorie name start with 'super'.
+
+Os dados e tabelas estao no beecrowd*/
 
 SELECT products.id, products."name"
 FROM products
